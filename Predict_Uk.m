a747b729795ca12f72fecf44bfc2c2c1d777d416
Jessica Tierney <jess.tierney@gmail.com>
2018-03-08 14:28:17 -0700

    Add files via upload

--- Predict_Uk.m
+++ Predict_Uk.m
@@ -1,14 +1,23 @@
 function output=Predict_Uk(age,uk,pstd)
 %
+%BAYSPLINE: a Bayesian b-spline calibration for the alkenone
+%paleothermometer. Predicts values of SST from measurements of uk37'.
+%Please cite the source publication when using this calibration:
+%
+%Tierney, JE and Tingley, MP (2018). BAYSPLINE: A new calibration for the
+%alkenone paleothermometer. Paleoceanography and Paleoclimatology, 33. https://doi.org/10.1002/2017PA003201
+%
 %INPUTS:
 %age = vector of depth or age values of length N
 %uk = uk37' values of length N
-%pstd = prior standard deviation (scalar). Recommended values are 7-10 for most uk
-%timeseries. Lower values OK for timeseries with smaller range. 
+%pstd = prior standard deviation (scalar). Recommended value: 10. This 
+%conservative value that works for most applications.
+%At high UK, a more restrictive value of 5 may be desirable to
+%assign a low probability to SSTs > 35C (Unlikely given that UK approaches 1 near this value). 
 
 %OUTPUTS:
-%output.prior_mean = Prior mean value, taken from the mean of the UK timeseries
-%converted to SST with the Prahl equation.
+%output.prior_mean = Prior mean value, taken from the UK timeseries
+%converted to SST with the Prahl '88 culture equation.
 %
 %output.prior_std = Prior standard deviation (user set).
 %
@@ -16,55 +25,60 @@ function output=Predict_Uk(age,uk,pstd)
 %Values are chosen to achieve a acceptance rate of ca. 0.44
 %(Gelman, 2003).
 %
+%output.rhat = Rhat statistic to assess convergence. Should be close to 1.
+%
 %output.SST = 5 x N vector of inferred SSTs, includes 5% level (lower 2sigma), 16% level
 %(lower 1sigma), 50% level (median values), 84% level (upper 1sigma), and
 %95% level (upper 2 sigma).
+%
+%output.ens = full ensemble (N = 2500) of posterior SSTs.
 %
 %PLOTS:
 %1) Prior distribution vs posterior distribution. Prior should be wider
 %unless a strong control is warranted.
 %
 %2) Time series converted to SST values, with 1-sigma confidence intervals.
-
+%
+%For a full explanation of the approach see Tierney & Tingley (2018).
 %% load model parameters
 load bayes_posterior_v2.mat;
 
-%thin draws a bit
+%thin the posterior draws a bit
 b_draws_final=b_draws_final(1:3:end,:);
 tau2_draws_final=tau2_draws_final(1:3:end);
 
-%set UK37 obs
+%confirm UK obs are column vector
 uk=uk(:);
 
 N_Ts=length(uk);
 N_p=length(tau2_draws_final);
-%Nsamps
+%number of samples
 N=500;
+%burnin to discard
 burnin=250;
 
-%set priors. Use prahl conversion to target mean and std
-%pm=median((uk-.039)./.034);
-pm=(uk-.039)./.034;
-%vectorize priors
-%prior_mean=pm*ones(N_Ts,1);
-prior_mean=pm;
+%set priors. Use Prahl 88 to create a prior mean vector
+prior_mean=(uk-.039)./.034;
+%prior variance comes from user-defined pstd
 prior_var=pstd^2*ones(N_Ts,1);
 %save priors to output
-output.prior_mean=pm;
+output.prior_mean=prior_mean;
 output.prior_std=pstd;
 
-%set an initial SST value
-init=pm;
+%set the initial SST values to prior mean
+init=prior_mean;
 
+%create empty matrices of the correct size
 MH_samps_t=NaN(N_Ts,N_p,N-burnin);
 accepts_t=NaN(N_Ts,N_p,N-burnin);
 
 %make a spline with set knots
-order=3;%spline order, 3 for quadratic
+order=3; %spline order
 kn = augknt(knots,order); %knots
 
-%assign width of jumping distribution
-pmm=median(pm);
+%assign width of jumping distribution based on average UK temp to obtain
+%40-50% accept rate
+pmm=median(prior_mean);
 if pmm<20
     JW=3.5;
     elseif pmm>=20 && pmm <= 23.7
@@ -106,7 +120,7 @@ parfor jj=1:N_p
             pr_now=normpdf(s_prop,prior_mean,sqrt(prior_var));
             %multiply to get proposal S0_p
             S0_p=LL_now.*pr_now;
-
+            %calculate MH ratio
             MH_rat=S0_p./S0_now;
             success_rate=min(1, MH_rat);
 
@@ -139,7 +153,7 @@ MH_s=sort(MH_c,2);
 pers5=round([.05 .16 .5 .84 .95].*size(MH_c,2));
 output.SST=MH_s(:,pers5);
 %%
-%take a subsample of MH to work with for ks.
+%take a subsample of MH to work with for ksdensity.
 MH_sub=MH_c(:,1:50:end);
 output.ens=MH_sub;
 %plot prior vs post
@@ -164,3 +178,41 @@ set(f2,'pos',[550 700 500 400]);
   hold on;
  plot(age,output.SST(:,4),'color',[.4 .4 .4],'linewidth',1,'linestyle','--');
  set(gca,'xlim',[min(age) max(age)]);
+function [Rhat, Neff]=ChainConvergence(chains, M)
+%
+% function [Rhat, Neff]=ChainConvergence(chains, M)
+% 
+% calculate the R-hat statistic for
+% monitoring  convergence of multiple parallel MCMC runs. Also outputs
+% n_eff.. 
+% chains: a matrix of MCMC chains, each of the same length. 
+% M: the number of different chains - must be one of the dimensions of
+% chains. 
+%
+
+
+if ismember(M, size(chains))==0
+    disp('Error: Second input must be the number of chains, so one of the dimensions of the first input.')
+    return
+elseif length(chains(:,1))==M
+    chains=chains';
+end
+
+% each column is a chain. 
+N=length(chains(:,1));
+
+%quantities from Gelman:
+
+psi_bar_dot_j = mean(chains);
+psi_bar_dot_dot=mean(psi_bar_dot_j);
+
+B= (N/(M-1))*sum((psi_bar_dot_j-psi_bar_dot_dot).^2);
+
+s2_j= (1/(N-1))*sum((chains-kron(psi_bar_dot_j, ones(N,1))).^2);
+
+W=(1/M)*sum(s2_j);
+
+var_hat_pos=((N-1)/N)*W + (1/N)*B;
+
+Rhat=sqrt(var_hat_pos/W);
+Neff=M*N*min(var_hat_pos/B, 1);
